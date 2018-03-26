56ec43cab2c8553a84523ecc0583bb119d142b0e
EliranG <eliran.geffen@gmail.com>
2018-03-26 20:41:55 +0300

    a

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,154 +1,109 @@
 const express = require("express");
 const apiRouter = express.Router();
-const oneSignalAgent = require("../notifications/oneSiganlAgent");
 
-var router = function (mongoose) {
+var router = function(mongoose) {
   var models = require("../database/db.models")(mongoose);
 
-  apiRouter.route("/journey/list/:memberId/:isExpert*?").get(function (request, response) {
-    if (request.params.memberId <= 0) {
+  apiRouter.route("/test").get(function(request, response) {
+    response.send("OK");
+  });
+
+  apiRouter.route("/db/get").get(function(request, response) {
+    models.Test.find({}, function(err, testItems) {
+      if (err) console.log(err);
+      else {
+        response.send({ testItems: testItems });
+      }
+    });
+  });
+
+  apiRouter.route("/db/insert").get(function(request, response) {
+    var newTest = new models.Test({
+      name: "name " + new Date(),
+      text: "text"
+    });
+
+    models.Test.create(newTest, function(err, item) {
+      if (err) console.log(err);
+      else {
+        console.log("Inserted: " + item);
+        response.send("Inserted: " + item);
+      }
+    });
+  });
+  
+
+
+  
+
+  apiRouter.route("/journey/list/:memberId/:isExpert*?").get(function(request, response){
+    if(request.params.memberId <=0){
       response.send({});
       return;
     }
 
-    let userId = parseInt(request.params.memberId, 10);
-    let isExpert = request.params.isExpert == 'true' ? true : false;
+    let userId = parseInt(request.params.memberId, 10) ;
+    let isExpert = request.params.isExpert=='true'?true: false;
 
-    var res;
-    let findSet = isExpert?{"expertID":userId}: {"clientID":userId};
-    let querey = isExpert?{clientID:1, qty: 1, _id:0}: {expertID:1, qty: 1, _id:0};
-    models.Journey.find(findSet, querey , function(err, items){
-      if (err) {
-        console.log(err);
-        response.send({});
-      }
-      else {
-        response.send({"userId":userId, "partners":items});
-      }
-  });
-});
+    response.send({"userId":userId, "partners":[123430, 248478, 1665189, 63837]});
+    return;
 
-  var findByClientIDAndExpertID = function (findParams, callback) {
-    models.Journey.findOne({ "clientID": findParams.clientID, "expertID": findParams.expertID }, function (err, doc) {
-      callback(err, doc);
-    });
-  };
-
-
-  // Print the Journey of a specific Client & Expert
-  apiRouter.route("/journey/:memberId/:expertId").get(function (request, response) {
-    var doc = findByClientIDAndExpertID(
-      {
-        clientID: request.params.memberId,
-        expertID: request.params.expertId
-      },
-      function (err, doc) {
-        if (err) 
-          console.log(err);
-        response.send({ "journey": doc });
-      });
   });
 
-  // Update Journey data list
-  apiRouter.route("/journey/update").post(function(request, response){    
-    var query = {clientID: request.body.memberId, expertID: request.body.expertId};    
-    var update = {'$push': {journeyDataList:createJourneyDataList( request.body.title,
-      request.body.text,
-      request.body.date,
-      request.body.isReminder,
-      request.body.status,
-      request.body.initiator)}};
-    var ret = models.Journey.findOneAndUpdate(query,update, {upsert: true}, function(err, res) {//findAndModify({"query":query}, [], {"update":update}, function(err) {
-     if (err) { 
-         throw err;
-     }
-     else { 
-           console.log("updated!");
-           response.send("OK");
-          }
-      });    
-    });
-/*
-    models.Journey.update(
-    {
-      "clientID":request.params.memberId, 
-      "expertID":request.params.expertId,
-      $push: (request.params.journeyDataList)
-    } ,
-    function(err, items){
-      if (err) console.log(err);
-      else {
-              response.send({"journey":items});
-           }
-      });
-    });
-*/
-
-  
   apiRouter.route("/journey/:memberId/:expertId").get(function(request, response){
-    var res;
-    models.Journey.find({"clientID":request.params.memberId, "expertID":request.params.expertId}, function(err, items){
-      if (err) console.log(err);
-      else {
-        response.send({"journey":items});
-      }
-    });
+    response.send(new entry(parseInt(request.params.memberId),parseInt(request.params.expertId)));
   });
   
-/*
-  apiRouter.route("/journey/inseret").post(function (request, response) {
+  apiRouter.route("/journey/update").post(function(request, response){
     let memberId = parseInt(request.body.memberId);
     let expertId = parseInt(request.body.expertId);
-    oneSignalAgent.sendNotification(request.body.memberId,request.body.date,request.body.expertId);
+
     saveToDb(memberId,
-      expertId,
-      request.body.title,
+      expertId, 
+      request.body.title, 
       request.body.text,
       request.body.date,
       request.body.isReminder,
       request.body.status,
       request.body.initiator);
 
-    response.send("OK");
+      response.send("OK");
   });
-*/
-  var createJourneyDataList = function(title, text, reminderDate,isReminder, status, expertIsInitiator){
+
+  var saveToDb = function(memberId, expertId,title,text, reminderDate, isReminder, status, expertIsInitiator){
     var journeyDataList = new models.JourneyDataList({
       title: title,
-      text: text,
-      reminderDate: reminderDate,
-      isReminder: isReminder,
-      status: status,
-      expertIsInitiator: expertIsInitiator
+    text: text,
+    reminderDate: reminderDate,
+    isReminder: isReminder,
+    status: status,
+    expertIsInitiator:expertIsInitiator 
     });
-    return journeyDataList;
-  };
-
-  var saveToDb = function (memberId, expertId, title, text, reminderDate, isReminder, status, expertIsInitiator) {
-    
     var entry = new models.Journey({
       clientID: memberId,
       expertID: expertId,
-      journeyDataList: [createJourneyDataList(title, text, reminderDate,isReminder, status, expertIsInitiator)]
-    });
+      journeyDataList: [journeyDataList]
+  });
 
-    entry.save(function (err) {
+  entry.save(function (err) {
       if (err) {
-        var errMsg = 'Error saving the Journey.' + err;
-        res.render('newJourney', { title: 'Journey - New Journey (error)', message: errMsg });
+          var errMsg = 'Error saving the Journey.' + err;
+          res.render('newJourney', { title: 'Journey - New Journey (error)', message: errMsg });
       }
       else {
-        console.log('Journey was saved!');
+          console.log('Journey was saved!');
+          // Redirect to the home page to display list of notes...
+          // res.send("Inserted: " + res );
       }
-    });
+  });        
   };
  
   apiRouter.route("/pushUser/").post(function(request, response){
 
     let clientID = parseInt(request.body.clientID);
-    let oneSignalUserId = parseInt(request.body.oneSignalUserId);
+    let oneSignalUserId = request.body.oneSignalUserId;
 
-    var pushUser = new models.pushUser({
+    var pushUser = new models.PushUser({
       clientID: clientID,
       oneSignalUserId:oneSignalUserId
     });
@@ -156,14 +111,32 @@ var router = function (mongoose) {
   pushUser.save(function (err) {
       if (err) {
           var errMsg = 'Error saving push user: ' + err;
-          res.render('newPushUser', { title: 'saving push user (error)', message: errMsg });
+          response.render('newPushUser', { title: 'saving push user (error)', message: errMsg });
       }
       else {
           console.log('Push user was saved!');
-           res.send("New PushUserInserted: " + clientID );
+          response.send("New PushUserInserted: " + clientID );
       }
-  });        
+  
   });
+});
+//   var entry = function(memberId, expertId, title, text, date, isReminder,status,initiator){
+//    return {
+//     "memberId":memberId,
+//     "expertId": expertId,
+//     "journeyDataList":[
+//       {
+//         "title":title,
+//         "text":text,
+//         "createdDate":Date.now,
+//         "date": date,
+//         "isReminder": isReminder,
+//         "status":status,
+//         "initiator": initiator
+//       }
+//     ]
+//   };
+// };
 
   return apiRouter;
 };
