51f55de5f793c34c878925714b1993814ae03069
yan101 <yanivrah@gmail.com>
2018-03-26 21:19:05 +0300

    commit

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const apiRouter = express.Router();
+const oneSignalAgent = require("../notifications/oneSiganlAgent");
 
 var router = function(mongoose) {
   var models = require("../database/db.models")(mongoose);
@@ -57,7 +58,7 @@ var router = function(mongoose) {
   apiRouter.route("/journey/update").post(function(request, response){
     let memberId = parseInt(request.body.memberId);
     let expertId = parseInt(request.body.expertId);
-
+    oneSignalAgent.sendNotification(request.body.memberId,request.body.date,request.body.expertId);
     saveToDb(memberId,
       expertId, 
       request.body.title, 
