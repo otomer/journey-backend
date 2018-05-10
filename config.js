bc6715e06e8ff070d33a316c0d41a1b4eb53db59
Tomer <o.tomerr@gmail.com>
2018-05-10 07:16:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 module.exports = {
   server: {
-    port: process.env.PORT || 3030,
+    port: process.env.PORT || "<port>,
     host: process.env.HOST || "localhost"
   },
   database: {
     url: "mongodb://<user>:<password>@<host>:<port>/<database>",
-    user: process.env.DB_USER || "jadmin",
-    password: process.env.DB_PASS || "jadmin",
-    host: "ds223019.mlab.com",
-    port: "23019",
-    database: process.env.DB_NAME || "journey-db"
+    user: process.env.DB_USER || "<user>",
+    password: process.env.DB_PASS || "<password>",
+    host: "<host>",
+    port: "<port>",
+    database: process.env.DB_NAME || "<database>"
   },
   pushNotifications: {
-    appID: "df41d13a-481d-463a-88a1-d1f69a4e5c38",
-    ApiKey: "MjRjZTc0NTAtYjdkNC00YTdiLWI5OWItNzljOTIwZDM5NTc2" 
+    appID: "<appID>",
+    ApiKey: "<ApiKey>" 
   }
 };
